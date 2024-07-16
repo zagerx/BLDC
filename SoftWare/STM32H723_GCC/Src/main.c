346e751ad5f6ebed3aacb0a189e6709583910fd4
zager <1252496768@qq.com>
2024-07-16 19:51:39 +0800

    20240716 修改H7

--- SoftWare/STM32H723_GCC/Src/main.c
+++ SoftWare/STM32H723_GCC/Src/main.c
@@ -148,6 +148,7 @@ int main(void)
   MX_ADC1_Init();
   MX_TIM4_Init();
   MX_USART2_UART_Init();
+  MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
   USER_DEBUG_NORMAL("H7 hello word\r\n");
   /* USER CODE END 2 */
