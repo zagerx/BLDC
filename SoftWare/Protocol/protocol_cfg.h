9a1400fd2915df8b9be920a2202c919777cc7e88
zager <1252496768@qq.com>
2024-07-25 12:00:52 +0800

    protocol init commit

--- SoftWare/Protocol/protocol_cfg.h
+++ SoftWare/Protocol/protocol_cfg.h
@@ -1,32 +1,6 @@
 #ifndef __PROTOCOL_CFG__H
 #define __PROTOCOL_CFG__H
 
-#include "stdlib.h"
-#include "string.h"
-
-#include "macro_defined.h"
-#include "crc.h"
-#include "fifo.h"
-#include "ipc.h"
-#include "list.h"
-#include "fsm.h"
-#include "debuglog.h"
-#include "heap.h"
-
-
-#undef NULL
-#define NULL 0
-#undef bool
-#define bool	
-#undef true
-#define true	1
-#undef false
-#define false	0
-
-
-#define PRO_FRAME_MIN_SIZE  8
-#define	PRO_FRAME_MAX_SIZE	256
-#define PRO_FIFO_SIZE PRO_FRAME_MAX_SIZE * (4)
 
 
 #endif
